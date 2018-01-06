60254a1fa484cdfe1c60bafbfa0fe7d13f47dc96
johnmccain <johnm.freestate@gmail.com>
2018-01-05 21:03:43 -0600

    Add ServoMessage and ServoMessageManager Integrate ServoMessageManager into Actuator for servo communication

--- Actuator.py
+++ Actuator.py
@@ -1,12 +1,11 @@
 """
-Basic mock class for a robotic actuator
 Does not store significant amounts of metadata about the actuator as this should be managed in the interpreter
 """
 
 import uuid
 import serial
 
-
+from ServoMessageManager import ServoMessageManager
 
 class Actuator:
     def __init__(self,
@@ -15,7 +14,7 @@ class Actuator:
                  defaultValue=0,
                  expirationBehavior="dynamic",
                  _id=None,
-                 speed=500):
+                 speed=None):
         if _id is None:
             self.uuid = uuid.uuid4()
         else:
@@ -25,6 +24,7 @@ class Actuator:
         self.value = defaultValue
         self.defaultValue = defaultValue
         self.expirationBehavior = expirationBehavior
+        self.servoMessageManager = ServoMessageManager()
     
     def __str__(self):
         obj = {
@@ -55,63 +55,12 @@ class Actuator:
     @value.setter
     def value(self, value):
         if self.acceptableValue(value):
-            # TODO: should send message to servo with updated value if set
+            # send message to servo if value was updated
             if not hasattr(self, "_value") or self._value != value:
                 self._value = value
-                # maybe call a function that sends the servo message here?
-                self.send_servo(value)
+                message = self.servoMessageManager.getMessage()
+                message.build(self.channel, self._value, speed=self.speed)
+                message.send()
         else:
-            # This situation should always be prevented by the interpeter
+            # TODO: Interpreter should handle exception being thrown
             raise Exception("Value %d is not within acceptable range for actuator %s" % (value, self.uuid))
-
-
-
-    def send_servo(self, serial_conn, value):
-        st_cmd = b"#{} P{} S1000\r".format(self.channel, value)
-        #st_cmd = str('#7 P' + value + ' S1000\r')
-        serial_conn.write(st_cmd)
-
-
-if __name__ == "__main__":
-    import time
-
-    # Holds actuator objects
-    myActuators = []
-
-    # Single serial connection
-    s = serial.Serial('/dev/ttyUSB0')
-    if s is None:
-            raise IOError("Serial Connection Failed!")
-
-    # Instantiation
-    for channel in range(16):
-        testClass = Actuator(channel,
-                             valueRange={"gte": 500, "lte": 2500},
-                             defaultValue=1500,
-                             expirationBehavior="dynamic",
-                             _id=None,
-                             speed=500)
-        myActuators.append(testClass)
-
-    # Dump actuator stats as they would appear in the actuator list
-    for actuator in myActuators:
-        print(actuator)
-
-    testPattern = [100*x for x in range(5, 26)]
-
-    #testPattern = []
-    #for x in range(5, 26):
-    #    testPattern.append(100 * x)
-    
-    for actuator in myActuators:
-        print("Testing servo:")
-        print("\t{}".format(actuator))
-        # Issue test across full range of motion
-        for testValue in testPattern:
-            actuator.send_servo(s, testValue)
-            time.sleep(0.5)
-        
-        # Center servo before moving on
-        actuator.send_servo(s, 1500)
-
-
---
+++ ServoMessageManager.py
@@ -0,0 +1,107 @@
+import threading
+import queue
+import serial
+import time
+
+from ServoMessage import ServoMessage
+
+class ServoMessageManager(object):
+    """
+    Singleton manager for sending servo messages
+    Must call stop() before discarding
+    """
+
+    instance = None
+
+    class __ServoMessageManager:
+        def __init__(self, interval=0.01):
+            self.lock = threading.Lock()
+            self.queue = queue.Queue()
+            self.serial = serial.Serial('/dev/ttyUSB0')
+            if self.serial is None:
+                raise IOError("Serial Connection Failed!")
+            self.stopped = False
+            self.target = time.time()
+            self.interval = interval
+            self.threads = []
+            t = threading.Thread(target=self.__consume)
+            self.threads.append(t)
+            t.start()
+
+        def stop(self):
+            self.queue.join()
+            self.stopped = True
+            self.joinThreads(block=True, timeout=None)
+
+        def joinThreads(self, block=False, timeout=0):
+            # try to acquire lock, just skips if block is False
+            if self.lock.acquire(blocking=block):
+                threads = list(self.threads)
+                self.lock.release()
+            else:
+                return
+
+            for thread in threads:
+                if thread == threading.current_thread():
+                    continue
+                thread.join(timeout=timeout)
+                if not thread.is_alive():
+                    self.threads.remove(thread)
+
+        def __consume(self):
+            messages = []
+            # wait until the queue isn't empty
+            while not self.queue.empty():
+                messages.append(self.queue.get_nowait())
+                self.queue.task_done()
+            if messages:
+                self.sendMessages(messages)
+            self.joinThreads()
+
+            if not self.stopped:
+                self.target += self.interval
+                delaySeconds = self.target - time.time()
+                timerthread = threading.Timer(delaySeconds, self.__consume)
+                timerthread.start()
+                with self.lock:
+                    self.threads.append(timerthread)
+
+        
+        def getMessage(self):
+            return ServoMessage(self)
+
+        def sendMessages(self, messages):
+            # select only last message received per channel
+            sendableMessages = {}
+            for message in messages:
+                sendableMessages[message.channel] = message
+            messages = sendableMessages.values()
+
+            channel_messages = []
+            for message in messages:
+                channel_message = b"#{} P{}".format(message.channel, message.value)
+                if message.time:
+                    channel_message += b" T{}".format(message.time)
+                elif message.speed:
+                    channel_message += b" S{}".format(message.speed)
+            serial_message = b" ".join(channel_messages) + b"\r"
+            self.serial.write(serial_message)
+
+
+        def enqueue(self, message):
+            """
+            Adds a ServoMessage to the queue (non blocking)
+            """
+            t = threading.Thread(target=self.__enqueue, args = (message,))
+            t.start()
+
+        def __enqueue(self, message):
+            self.queue.put(message)
+            with self.lock:
+                self.threads.append(threading.current_thread())
+
+    def __new__(cls):
+        if not ServoMessageManager.instance:
+            ServoMessageManager.instance = ServoMessageManager.__ServoMessageManager()
+        return ServoMessageManager.instance
+
