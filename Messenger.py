59a3fc827737013f4b0007ada56f78974fde3ecc
pcm <pcm@ku.edu>
2018-03-25 23:39:58 -0500

    Allow graceful exits and other bug fixes

--- Messenger.py
+++ Messenger.py
@@ -14,6 +14,14 @@ import CommandMessage as CM
 import AcknowledgementMessage as AM
 import ReportMessageGenerator as RMG
 
+
+#Example Termination Message
+tM = {
+      "message_id": "067c8c59-710a-4c15-8265-b7f1e49b828c",
+      "message_type": "termination",
+      "timestamp": 1509748526.3482552
+}
+
 class OfflineServerError(Exception):
     pass
 
@@ -36,6 +44,7 @@ class Messenger:
         self.context = None
         self.auth = None
         self.client = None
+        self.server_side_termination = False
 
     def __enter__(self):
         print("ENTER FUNCTION")
@@ -50,32 +59,51 @@ class Messenger:
         self.client.curve_secretkey = self.client_secret
         self.client.curve_publickey = self.client_public
         self.client.curve_serverkey = self.server_public
+        self.client.setsockopt(zmq.LINGER, 100)
         self.client.connect(config.SERVER_ENDPOINT)
 
         self.poll = zmq.Poller()
-        self.poll.register(self.client, zmq.POLLIN)
+        self.poll.register(self.client, zmq.POLLIN | zmq.POLLOUT)
         return self
 
 
     def __exit__(self, exception_type, exception_value, traceback):
+        if(self.is_current(1)):
+            print("Was current")
+            self.client.recv_json()
         print("EXITING WITH")
         self.auth.stop()
         print("stop auth")
-        self.client.close()
+        while(not self.client.closed):
+            self.client.disconnect(config.SERVER_ENDPOINT)
+            self.client.close()
+        print("socket closed")
+        self.context.destroy(linger=1)
         print("closed client")
-        self.context.term()
-        print("terminated context")
 
     def send(self,message):
-        print("SEND START")
-        if(self.send_message_type == "command"):
-            self.client.send_json(CM.CommandMessage(message).json())
-        elif(self.send_message_type == "report"):
-            self.client.send_json(message)
-            # self.client.send_json(RM.ReportMessage(message).json())
-        elif(self.send_message_type == "acknowledgement"):
-            self.client.send_json(AM.AcknowledgementMessage(message).json())
-        print("SEND END")
+        if(not self.is_ready_to_send(timeout=1)):
+            print("Not ready to send")
+        else:
+            print("SEND START")
+            print("MESSAGE TYPE IS ", message["message_type"])
+            try:
+                if(message["message_type"] == "termination"):
+                    self.client.send_json(message, zmq.NOBLOCK)
+                    print("Termination Message Sent")
+                elif(self.send_message_type == "command"):
+                    self.client.send_json(CM.CommandMessage(message).json(), zmq.NOBLOCK)
+                    print("Command Message Sent")
+                elif(self.send_message_type == "report"):
+                    self.client.send_json(message, zmq.NOBLOCK)
+                    print("Report Message Sent")
+                    # self.client.send_json(RM.ReportMessage(message).json())
+                elif(self.send_message_type == "acknowledgement"):
+                    self.client.send_json(AM.AcknowledgementMessage(message).json(), zmq.NOBLOCK)
+                    print("Acknowledgement Message Sent")
+            except:
+                print("Couldn't send yet")
+            print("SEND END")
 
     def try_resend(self, message, retries_left):
         print("RESEND START")
@@ -87,22 +115,30 @@ class Messenger:
         self.poll.unregister(self.client)
         if retries_left == 0:
             logging.error("E: Server seems to be offline, abandoning")
-            raise OfflineServerError("E: Server seems to be offline, abandoning")
-        logging.info("I: Reconnecting and resending (%s)" % message)
-        # Create new connection
-        self.client = self.context.socket(zmq.REQ)
-        self.client.connect(config.SERVER_ENDPOINT)
-        self.poll.register(self.client, zmq.POLLIN)
-        self.send(message)
-        print("RESEND END")
+            # raise OfflineServerError("E: Server seems to be offline, abandoning")
+        else:
+            logging.info("I: Reconnecting and resending (%s)" % message)
+            # Create new connection
+            self.client = self.context.socket(zmq.REQ)
+            self.client.setsockopt(zmq.LINGER, 100)
+            self.client.connect(config.SERVER_ENDPOINT)
+            self.poll.register(self.client, zmq.POLLIN | zmq.POLLOUT)
+            self.send(message)
+            print("RESEND END")
         return retries_left
 
+    def send_termination(self):
+        self.send(tM)
 
     def recv(self):
         print("RECEIVE START")
         message = 0
-        if(self.recv_message_type == "command"):
-            message = CM.CommandMessage(self.client.recv_json())
+        received_message = self.client.recv_json()
+        if(received_message["message_type"] == "termination"):
+            message = tM
+            self.server_side_termination = True
+        elif(self.recv_message_type == "command"):
+            message = CM.CommandMessage(received_message)
         elif(self.recv_message_type == "acknowledgement"):
             message = AM.AcknowledgementMessage(self.client.recv_json())
         reply = message.json()
@@ -110,9 +146,16 @@ class Messenger:
         print("RECEIVE END")
         return reply
 
-    def is_current(self):
+    def is_current(self, timeout=config.REQUEST_TIMEOUT):
         print("IS_CURRENT START")
-        socks = dict(self.poll.poll(config.REQUEST_TIMEOUT))
+        socks = dict(self.poll.poll(timeout))
         status = socks.get(self.client) == zmq.POLLIN
         print("IS_CURRENT END")
+        return status
+
+    def is_ready_to_send(self, timeout=config.REQUEST_TIMEOUT):
+        print("is_ready_to_send START")
+        socks = dict(self.poll.poll(timeout))
+        status = socks.get(self.client) == zmq.POLLOUT
+        print("is_ready_to_send END")
         return status
--- robot_client.py
+++ robot_client.py
@@ -40,11 +40,18 @@ def commandMessageManager(actuatorList):
     last_timestamp = time.time()
     with Messenger("command", "acknowledgement") as m:
         retries_left = config.REQUEST_RETRIES
-        while retries_left:
+        #Example Termination Message
+        tM = {
+              "message_id": "067c8c59-710a-4c15-8265-b7f1e49b828c",
+              "message_type": "termination",
+              "timestamp": 1509748526.3482552
+        }
+        while retries_left > 0:
             try:
                 # time.sleep(10)
                 ######### EXAMPLE VALUES ########
                 request = {"message_id": last_message_id, "message_type": "acknowledgement","timestamp": last_timestamp}
+
                 # client.send(json.dumps(request))
                 # client.send_json(request)
                 print("SENDING Acknowledgement")
@@ -64,27 +71,47 @@ def commandMessageManager(actuatorList):
                         print("I: Server replied with message (%s)" % reply)
                         last_timestamp = time.time()
                         last_message_id = reply["message_id"]
-                        parsed_dict = {"message_id": reply["message_id"], "message_type": reply["message_type"],
-                                       "robot_id": reply["robot_id"], "configuration_id": reply["configuration_id"],
-                                       "session_id": reply["session_id"], "timestamp": reply["timestamp"],
-                                       "instructions": list()}
-                        for i in reply["instructions"]:
-                            parsed_dict["instructions"].append(i)
+                        if(reply["message_type"] == "command"):
+                            parsed_dict = {"message_id": reply["message_id"], "message_type": reply["message_type"],
+                                           "robot_id": reply["robot_id"], "configuration_id": reply["configuration_id"],
+                                           "session_id": reply["session_id"], "timestamp": reply["timestamp"],
+                                           "instructions": list()}
+                            for i in reply["instructions"]:
+                                parsed_dict["instructions"].append(i)
 
-                        # ###### CALL INTERPRETTER PASSING parsedDict - PLEASE CORRECT IF WRONG, don't know how its structured ######
-                        interpreter.interpret(parsed_dict)
-
+                            # ###### CALL INTERPRETTER PASSING parsedDict - PLEASE CORRECT IF WRONG, don't know how its structured ######
+                            interpreter.interpret(parsed_dict)
+                        # elif(reply["message_type"] == "termination"):
+                        #     print("Termination Message Received, Exiting!")
+                        #     while(True):
+                        #         print("waiting")
+                        #     break
                         retries_left = config.REQUEST_RETRIES
                         expect_reply = False
 
                     else:
                         try:
                             retries_left = m.try_resend(request, retries_left)
-                        except OfflineServerError:
-                            break
+                            if(retries_left <= 0):
+                                expect_reply = False
+                        # except OfflineServerError:
+                        #     print("THE SERVER WAS OFFLINE!!!!!")
+                        except:
+                            pass
+
+                            # break
             except KeyboardInterrupt:
-                interpreter.stop()
-                break
+                print(type(m))
+                # while(True):
+                #     print(type(m))
+                # if (expect_reply):
+                #     print("HOLD ON, I'M EXPECTING A REPLY REAL QUICK!")
+                #     reply = m.recv()
+                print("Exiting")
+                retries_left = 0
+        # m.send_termination()
+    print("GOT HERE!")
+    interpreter.stop()
 
 def reportMessageManager():
     #initialize first acknowledgement message_id and timestamp
@@ -140,5 +167,5 @@ def reportMessageManager():
             break
 
 
-reportMessageManager()
-# commandMessageManager(myDummyActuatorList)
+#reportMessageManager()
+commandMessageManager(myDummyActuatorList)
