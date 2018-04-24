d05d15af4cce4fb561106e16c3564065bb4f147a
pcm <pcm@ku.edu>
2018-04-24 08:04:30 -0500

    Update config.py to use UUID instead of string for robot_id

--- config.py
+++ config.py
@@ -12,4 +12,34 @@ SERVER_ENDPOINT = "tcp://localhost:5555"
 REQUEST_TIMEOUT = 2500
 # REQUEST_RETRIES = 30
 REQUEST_RETRIES = 100
-ROBOT_ID = "johnny-five"
+ROBOT_ID = "7c78d289-f63e-40e2-89fc-8b033b84668e"
+
+#EXAMPLE COMMAND FOR COPY AND PASTE
+# {
+#     "message_id": "067c8c59-710a-4c15-8265-b7f1e49b828c",
+#     "message_type": "command",
+#     "robot_id": "7c78d289-f63e-40e2-89fc-8b033b84668e",
+#     "timestamp": 1509748526.3482552,
+#     "configuration_id": "067c8c59-710a-4c15-8265-b7f1e49b828c",
+#     "session_id": "067c8c59-710a-4c15-8265-b7f1e49b828c",
+#     "instructions": [
+#         {
+#         "value": 0.10666666666666667,
+#         "actuator_id": "067c8c59-710a-4c15-8265-b7f1e49b828c",
+#         "ttl": 1.412,
+#         "type": "static"
+#         },
+#         {
+#         "value": 0.10666666666666667,
+#         "actuator_id": "067c8c59-710a-4c15-8265-b7f1e49b828c",
+#         "ttl": 1.412,
+#         "type": "static"
+#         },
+#         {
+#         "value": 0.10666666666666667,
+#         "actuator_id": "067c8c59-710a-4c15-8265-b7f1e49b828c",
+#         "ttl": 1.412,
+#         "type": "static"
+#         }
+#     ]
+#     }
